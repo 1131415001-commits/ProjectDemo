3122e5696c88374a80d14265c133655891dc136b
1131415001-commits <1131415001@mail.hwu.edu.tw>
2026-01-02 22:44:10 +0800

    遊戲不能玩了。傻眼

--- Assets/Scripts/UI/UIKeyCountCtrl.cs
+++ Assets/Scripts/UI/UIKeyCountCtrl.cs
@@ -1,4 +1,3 @@
-using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +9,13 @@ public class UIKeyCountCtrl : MonoBehaviour
     public Color got;
     public Color none;
 
-
     // Start is called before the first frame update
     void Start()
     {
         UpdateKeyUI();
+
         GameData.updateKey = UpdateKeyUI;
+
 UICutInCtrl.instance.StartInfo();
     }
 
